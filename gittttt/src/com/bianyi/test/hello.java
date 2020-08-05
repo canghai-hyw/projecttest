9e593c62ca6689fcf124c007edbd249bed109f2b
canghai <2986938831@qq.com>
2020-08-06 00:14:59 +0800

    初始化项目

--- gittttt/src/com/bianyi/test/hello.java
+++ gittttt/src/com/bianyi/test/hello.java
@@ -2,11 +2,13 @@ package com.bianyi.test;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("hello1");
+        System.out.println("hello1");
+        System.out.println("hello1");
+        System.out.println("hello1");
+        System.out.println("hello1");
+        System.out.println("hello1");
+        System.out.println("hello1");
+        System.out.println("hello1");
     }
 }
